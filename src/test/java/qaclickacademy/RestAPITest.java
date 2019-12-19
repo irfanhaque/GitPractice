786a21331daa4c5032b52cbfdb9a288c7833a41c
irfanhaque <irfanulhaque2526@gmail.com>
2019-12-19 08:40:17 +0530

    first commit

---
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	@Test
+	public void JiraDefect(){
+		
+		System.out.println("JiraDefect");
+	}
+	
+	@Test
+	public void JiraReq(){
+		System.out.println("Jira Requirement");
+	}
+
+
+}
